7fb17ab63cc84d1fef50b79c9984bb50e7c9d3ae
Joey Han <joeyhan@Joeys-MacBook-Pro-4.local>
2022-11-26 18:17:02 -0500

    deploy

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ app.use('/campgrounds', campgroundsRoutes)
 app.use('/campgrounds/:id/reviews', reviewsRoutes)
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('Home')
 })
 
 app.all('*', (req, res, next) => {
