bd68e3b1db5746fdf6030873ebcb428918ca852b
Heung Joey Han <113553347+heungjhan@users.noreply.github.com>
2023-03-07 17:05:13 -0500

    Update index.js

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl);
